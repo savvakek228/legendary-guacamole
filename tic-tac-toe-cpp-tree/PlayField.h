9ec74d23573abeffa25c48b359aefcbeedb0c742
savva.kamenskih <savva_kam@mail.ru>
2020-05-20 14:43:03 +0500

    commit 2

---
+++ tic-tac-toe-cpp-tree/PlayField.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+
+#include <vector>
+
+class PlayField {
+public:
+
+    class CellPos {
+    public:
+        explicit CellPos(int cellNum);
+
+        CellPos(int row, int column);
+
+        int getRow() const;
+
+        void setRow(int value);
+
+        int getColumn() const;
+
+        void setColumn(int value);
+
+        explicit operator int() const;
+    private:
+        int _row;
+        int _column;
+    };
+
+    enum class CellStatus
+    {
+        csEmpty, csCross, csNought
+    };
+
+    enum class FieldStatus
+    {
+        fsInvalid, fsNormal, fsCrossesWin, fsNoughtsWin, fsDraw
+    };
+
+    explicit PlayField(std::vector<CellStatus>& field);
+
+    CellStatus operator[](const CellPos &cellPos) const;
+
+    std::vector<CellPos> getEmptyCells() const;
+
+    FieldStatus checkFieldStatus() const;
+
+    PlayField makeMove(const CellPos &cellPos) const;
+
+private:
+    static constexpr int fieldSize = 3;
+
+    bool hasWin(CellStatus player, bool row) const;
+
+    bool checkWinStatus(CellStatus player,bool row) const;
+
+    int countCells(CellStatus cellStatus) const;
+
+    PlayField operator+(const CellPos &cellPos) const;
+
+    std::vector<CellStatus> &_field;
+
+    bool checkWinStatus(CellStatus status) const;
+};
+
---
+++ tic-tac-toe-cpp-tree/TreeNode.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "PlayField.h"
+
+class TreeNode {
+public:
+    explicit TreeNode(PlayField &currentField);
+
+    bool isTerminal() const;
+
+    void addChild(TreeNode &childNode);
+
+    TreeNode &operator[](int childIndex) const;
+
+    int childCount() const;
+
+    PlayField &value() const;
+
+private:
+    PlayField &currentField;
+
+    std::vector<std::reference_wrapper<TreeNode>> childNodes;
+
+    TreeNode *ancestorNode = nullptr;
+
+    int childQty() const;
+};
